91ca4cf397ab1cbf93df04dff84e2e259c97f41f
JayJayc <Jarrry.c@gmail.com>
2023-01-19 10:54:15 +1100

    Updated for vm test

--- experiments/run_test_vm.py
+++ experiments/run_test_vm.py
@@ -1,10 +1,7 @@
 from numpy import load, array, save
-from algorithms import notears, causal_discovery_rl, gran_dag, dag_gnn
 
 import os
 from networkx import to_numpy_matrix
-from cdt.causality.graph import CAM
-from cdt.causality.graph import SAM
 import cdt
 import time
 from pandas import DataFrame
@@ -23,6 +20,7 @@ def run_notears(data: array, output_path: str):
     print("=================")
     print("Running NOTEARS: ", output_path)
     print("=================")
+    from algorithms import notears
     notear_result_dir = os.path.join(output_path, "notears")
     notear_result_path = os.path.join(notear_result_dir, "result.npy")
     if not os.path.isdir(notear_result_dir):
@@ -52,6 +50,7 @@ def run_cam(data: array, output_path: str):
     print("=================")
     print("Running CAM: ", output_path)
     print("=================")
+    from cdt.causality.graph import CAM
     cam_result_dir = os.path.join(output_path, "cam")
     cam_result_path = os.path.join(cam_result_dir, "result.npy")
     if not os.path.isdir(cam_result_dir):
@@ -75,6 +74,7 @@ def run_sam(data: array, output_path: str):
     print("=================")
     print("Running SAM: ", output_path)
     print("=================")
+    from cdt.causality.graph import SAM
     sam_result_dir = os.path.join(output_path, "sam")
     sam_result_path = os.path.join(sam_result_dir, "result.npy")
     if not os.path.isdir(sam_result_dir):
@@ -98,6 +98,7 @@ def run_rl_bic(data_path: str, output_path: str, shape: tuple):
     print("=================")
     print("Running RL_BIC: ", output_path)
     print("=================")
+    from algorithms import causal_discovery_rl
     rl_bic_result_dir = os.path.join(output_path, "rl_bic")
     if not os.path.isdir(rl_bic_result_dir):
         os.mkdir(rl_bic_result_dir)
@@ -121,6 +122,7 @@ def run_dag_gnn(data_path: str, output_path: str, shape: tuple):
     print("=================")
     print("Running DAG_GNN: ", output_path)
     print("=================")
+    from algorithms import dag_gnn
     dag_gnn_result_dir = os.path.join(output_path, "dag_gnn")
     if not os.path.isdir(dag_gnn_result_dir):
         os.mkdir(dag_gnn_result_dir)
@@ -142,6 +144,7 @@ def run_gran_dag(data_path: str, output_path: str, shape: tuple):
     print("=================")
     print("Running GRAN_DAG: ", output_path)
     print("=================")
+    from algorithms import gran_dag
     gran_dag_result_dir = os.path.join(output_path, "gran_dag")
     if not os.path.isdir(gran_dag_result_dir):
         os.mkdir(gran_dag_result_dir)
@@ -162,97 +165,107 @@ def run_gran_dag(data_path: str, output_path: str, shape: tuple):
     print(timing)
 
 
-def run_test():
+def run_test(test_name, mech_name, algo_name):
     causal_mech_dirs = next(os.walk(DATA_DIR))[1]
     for causal_mech_dir in causal_mech_dirs:
-        if not os.path.isdir(RESULT_DIR):
-            os.mkdir(RESULT_DIR)
-        # Configure paths
-        causal_mech_data_dir = os.path.join(DATA_DIR, causal_mech_dir)
-        dataset_dirs = next(os.walk(causal_mech_data_dir))[1]
-        causal_mech_result_dir = os.path.join(RESULT_DIR, causal_mech_dir)
-        # Create result dir
-        if not os.path.isdir(causal_mech_result_dir):
-            os.mkdir(causal_mech_result_dir)
-        for dataset in dataset_dirs:
+        if mech_name == causal_mech_dir:
+            if not os.path.isdir(RESULT_DIR):
+                os.mkdir(RESULT_DIR)
             # Configure paths
-            dataset_dir = os.path.join(causal_mech_data_dir, dataset)
-            data_path = os.path.join(dataset_dir, "data1.npy")
-            dataset_result_dir = os.path.join(causal_mech_result_dir, dataset)
+            causal_mech_data_dir = os.path.join(DATA_DIR, causal_mech_dir)
+            dataset_dirs = next(os.walk(causal_mech_data_dir))[1]
+            causal_mech_result_dir = os.path.join(RESULT_DIR, causal_mech_dir)
             # Create result dir
-            if not os.path.isdir(dataset_result_dir):
-                os.mkdir(dataset_result_dir)
-            # Load test data
-            test_data = load(data_path)
+            if not os.path.isdir(causal_mech_result_dir):
+                os.mkdir(causal_mech_result_dir)
+            for dataset in dataset_dirs:
+                if dataset == test_name:
+                    print("Running ", mech_name, test_name, algo_name)
+                    # Configure paths
+                    dataset_dir = os.path.join(causal_mech_data_dir, dataset)
+                    data_path = os.path.join(dataset_dir, "data1.npy")
+                    dataset_result_dir = os.path.join(causal_mech_result_dir, dataset)
+                    # Create result dir
+                    if not os.path.isdir(dataset_result_dir):
+                        os.mkdir(dataset_result_dir)
+                    # Load test data
+                    test_data = load(data_path)
 
-            dag_gnn_result_dir = os.path.join(dataset_result_dir, "dag_gnn")
-            gran_dag_result_dir = os.path.join(dataset_result_dir, "gran_dag")
-            rl_bic_result_dir = os.path.join(dataset_result_dir, "rl_bic")
-            sam_result_dir = os.path.join(dataset_result_dir, "sam")
+                    dag_gnn_result_dir = os.path.join(dataset_result_dir, "dag_gnn")
+                    gran_dag_result_dir = os.path.join(dataset_result_dir, "gran_dag")
+                    rl_bic_result_dir = os.path.join(dataset_result_dir, "rl_bic")
+                    sam_result_dir = os.path.join(dataset_result_dir, "sam")
 
-            # # Run NoTears
-            # run_notears(data=test_data, output_path=dataset_result_dir)
+                    # # Run NoTears
+                    # run_notears(data=test_data, output_path=dataset_result_dir)
 
-            # # Run CAM (https://arxiv.org/abs/1310.1533)
-            # run_cam(data=test_data, output_path=dataset_result_dir)
+                    # # Run CAM (https://arxiv.org/abs/1310.1533)
+                    # run_cam(data=test_data, output_path=dataset_result_dir)
+                    if algo_name == "SAM":
+                        if not os.path.isdir(sam_result_dir):
+                            # Run SAM (https://arxiv.org/abs/1803.04929v5)
+                            run_sam(data=test_data, output_path=dataset_result_dir)
+                        else:
+                            if len(os.listdir(sam_result_dir)) == 0:
+                                # Run SAM (https://arxiv.org/abs/1803.04929v5)
+                                run_sam(data=test_data, output_path=dataset_result_dir)
 
-            if not os.path.isdir(sam_result_dir):
-                # Run SAM (https://arxiv.org/abs/1803.04929v5)
-                run_sam(data=test_data, output_path=dataset_result_dir)
-            else:
-                if len(os.listdir(sam_result_dir)) == 0:
-                    # Run SAM (https://arxiv.org/abs/1803.04929v5)
-                    run_sam(data=test_data, output_path=dataset_result_dir)
+                    if algo_name == "RL_BIC":
+                        if not os.path.isdir(rl_bic_result_dir):
+                            # Run RL-BIC
+                            run_rl_bic(
+                                data_path=dataset_dir,
+                                output_path=dataset_result_dir,
+                                shape=test_data.shape,
+                            )
+                        else:
+                            if len(os.listdir(rl_bic_result_dir)) == 0:
+                                # Run RL-BIC
+                                run_rl_bic(
+                                    data_path=dataset_dir,
+                                    output_path=dataset_result_dir,
+                                    shape=test_data.shape,
+                                )
 
-            if not os.path.isdir(rl_bic_result_dir):
-                # Run RL-BIC
-                run_rl_bic(
-                    data_path=dataset_dir,
-                    output_path=dataset_result_dir,
-                    shape=test_data.shape,
-                )
-            else:
-                if len(os.listdir(rl_bic_result_dir)) == 0:
-                    # Run RL-BIC
-                    run_rl_bic(
-                        data_path=dataset_dir,
-                        output_path=dataset_result_dir,
-                        shape=test_data.shape,
-                    )
+                    if algo_name == "GRAN_DAG":
+                        if not os.path.isdir(gran_dag_result_dir):
+                            # Run Gran Dag
+                            run_gran_dag(
+                                data_path=dataset_dir,
+                                output_path=dataset_result_dir,
+                                shape=test_data.shape,
+                            )
+                        else:
+                            if len(os.listdir(gran_dag_result_dir)) == 0:
+                                # Run Gran Dag
+                                run_gran_dag(
+                                    data_path=dataset_dir,
+                                    output_path=dataset_result_dir,
+                                    shape=test_data.shape,
+                                )
 
-            if not os.path.isdir(gran_dag_result_dir):
-                # Run Gran Dag
-                run_gran_dag(
-                    data_path=dataset_dir,
-                    output_path=dataset_result_dir,
-                    shape=test_data.shape,
-                )
-            else:
-                if len(os.listdir(gran_dag_result_dir)) == 0:
-                    # Run Gran Dag
-                    run_gran_dag(
-                        data_path=dataset_dir,
-                        output_path=dataset_result_dir,
-                        shape=test_data.shape,
-                    )
-
-            if not os.path.isdir(dag_gnn_result_dir):
-                # Run DAG-GNN
-                run_dag_gnn(
-                    data_path=dataset_dir,
-                    output_path=dataset_result_dir,
-                    shape=test_data.shape,
-                )
-            else:
-                if len(os.listdir(dag_gnn_result_dir)) == 0:
-                    # Run DAG-GNN
-                    run_dag_gnn(
-                        data_path=dataset_dir,
-                        output_path=dataset_result_dir,
-                        shape=test_data.shape,
-                    )
+                    if algo_name == "DAG_GNN":
+                        if not os.path.isdir(dag_gnn_result_dir):
+                            # Run DAG-GNN
+                            run_dag_gnn(
+                                data_path=dataset_dir,
+                                output_path=dataset_result_dir,
+                                shape=test_data.shape,
+                            )
+                        else:
+                            if len(os.listdir(dag_gnn_result_dir)) == 0:
+                                # Run DAG-GNN
+                                run_dag_gnn(
+                                    data_path=dataset_dir,
+                                    output_path=dataset_result_dir,
+                                    shape=test_data.shape,
+                                )
 
 
 if __name__ == "__main__":
     print("Running test")
-    run_test()
+    print('cmd entry:', sys.argv)
+    test_name = sys.argv[1]
+    mech_name = sys.argv[2]
+    algo_name = sys.argv[3]
+    run_test(test_name, algo_name)
