e72af72626eea7f48a1fd6312273baedb162a089
JayJayc <Jarrry.c@gmail.com>
2023-02-16 23:33:18 +1100

    Added new folder for edro

---
+++ experiments/experiment_1_variation_edro/run_test.sh
@@ -0,0 +1,329 @@
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py medium_selection_bias_1 mix_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_1 mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_1 mix_mechanism RL_BIC
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py medium_selection_bias_2 mix_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_2 mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_2 mix_mechanism RL_BIC
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py medium_selection_bias_3 mix_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_3 mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_3 mix_mechanism RL_BIC
+
+# Mix Mech unfaithful 1
+python ./../run_test_vm.py medium_unfaithful_1 mix_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_1 mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_1 mix_mechanism RL_BIC
+
+# Mix Mech unfaithful 2
+python ./../run_test_vm.py medium_unfaithful_2 mix_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_2 mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_2 mix_mechanism RL_BIC
+
+# Mix Mech unfaithful 3
+python ./../run_test_vm.py medium_unfaithful_3 mix_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_3 mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_3 mix_mechanism RL_BIC
+
+# Mix Mech confounder 1
+python ./../run_test_vm.py medium_confounder_1 mix_mechanism SAM
+python ./../run_test_vm.py medium_confounder_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_1 mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_1 mix_mechanism RL_BIC
+
+# Mix Mech confounder 2
+python ./../run_test_vm.py medium_confounder_2 mix_mechanism SAM
+python ./../run_test_vm.py medium_confounder_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_2 mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_2 mix_mechanism RL_BIC
+
+# Mix Mech confounder 3
+python ./../run_test_vm.py medium_confounder_3 mix_mechanism SAM
+python ./../run_test_vm.py medium_confounder_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_3 mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_3 mix_mechanism RL_BIC
+
+# Linear Mech
+# Linear Mech Select Bias 1
+python ./../run_test_vm.py medium_selection_bias_1 linear_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_1 linear_mechanism RL_BIC
+
+# Linear Mech Select Bias 2
+python ./../run_test_vm.py medium_selection_bias_2 linear_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_2 linear_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_2 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_2 linear_mechanism RL_BIC
+
+# Linear Mech Select Bias 3
+python ./../run_test_vm.py medium_selection_bias_3 linear_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_3 linear_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_3 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_3 linear_mechanism RL_BIC
+
+# Linear Mech unfaithful 1
+python ./../run_test_vm.py medium_unfaithful_1 linear_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_1 linear_mechanism RL_BIC
+
+# Linear Mech unfaithful 2
+python ./../run_test_vm.py medium_unfaithful_2 linear_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_2 linear_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_2 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_2 linear_mechanism RL_BIC
+
+# Linear Mech unfaithful 3
+python ./../run_test_vm.py medium_unfaithful_3 linear_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_3 linear_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_3 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_3 linear_mechanism RL_BIC
+
+# Linear Mech confounder 1
+python ./../run_test_vm.py medium_confounder_1 linear_mechanism SAM
+python ./../run_test_vm.py medium_confounder_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_1 linear_mechanism RL_BIC
+
+# Linear Mech confounder 2
+python ./../run_test_vm.py medium_confounder_2 linear_mechanism SAM
+python ./../run_test_vm.py medium_confounder_2 linear_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_2 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_2 linear_mechanism RL_BIC
+
+# Linear Mech confounder 3
+python ./../run_test_vm.py medium_confounder_3 linear_mechanism SAM
+python ./../run_test_vm.py medium_confounder_3 linear_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_3 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_3 linear_mechanism RL_BIC
+
+# Sigmoid Add Mech
+# Sigmoid Add Mech Select Bias 1
+python ./../run_test_vm.py medium_selection_bias_1 sigmoid_add_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_1 sigmoid_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_1 sigmoid_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_1 sigmoid_add_mechanism RL_BIC
+
+# Sigmoid Add Mech Select Bias 2
+python ./../run_test_vm.py medium_selection_bias_2 sigmoid_add_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_2 sigmoid_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_2 sigmoid_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_2 sigmoid_add_mechanism RL_BIC
+
+# Sigmoid Add Mech Select Bias 3
+python ./../run_test_vm.py medium_selection_bias_3 sigmoid_add_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_3 sigmoid_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_3 sigmoid_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_3 sigmoid_add_mechanism RL_BIC
+
+# Sigmoid Add Mech unfaithful 1
+python ./../run_test_vm.py medium_unfaithful_1 sigmoid_add_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_1 sigmoid_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_1 sigmoid_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_1 sigmoid_add_mechanism RL_BIC
+
+# Sigmoid Add Mech unfaithful 2
+python ./../run_test_vm.py medium_unfaithful_2 sigmoid_add_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_2 sigmoid_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_2 sigmoid_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_2 sigmoid_add_mechanism RL_BIC
+
+# Sigmoid Add Mech unfaithful 3
+python ./../run_test_vm.py medium_unfaithful_3 sigmoid_add_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_3 sigmoid_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_3 sigmoid_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_3 sigmoid_add_mechanism RL_BIC
+
+# Sigmoid Add Mech confounder 1
+python ./../run_test_vm.py medium_confounder_1 sigmoid_add_mechanism SAM
+python ./../run_test_vm.py medium_confounder_1 sigmoid_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_1 sigmoid_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_1 sigmoid_add_mechanism RL_BIC
+
+# Sigmoid Add Mech confounder 2
+python ./../run_test_vm.py medium_confounder_2 sigmoid_add_mechanism SAM
+python ./../run_test_vm.py medium_confounder_2 sigmoid_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_2 sigmoid_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_2 sigmoid_add_mechanism RL_BIC
+
+# Sigmoid Add Mech confounder 3
+python ./../run_test_vm.py medium_confounder_3 sigmoid_add_mechanism SAM
+python ./../run_test_vm.py medium_confounder_3 sigmoid_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_3 sigmoid_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_3 sigmoid_add_mechanism RL_BIC
+
+# Sigmoid Mix Mech
+# Sigmoid Mix Mech Select Bias 1
+python ./../run_test_vm.py medium_selection_bias_1 sigmoid_mix_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_1 sigmoid_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_1 sigmoid_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_1 sigmoid_mix_mechanism RL_BIC
+
+# Sigmoid Mix Mech Select Bias 2
+python ./../run_test_vm.py medium_selection_bias_2 sigmoid_mix_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_2 sigmoid_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_2 sigmoid_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_2 sigmoid_mix_mechanism RL_BIC
+
+# Sigmoid Mix Mech Select Bias 3
+python ./../run_test_vm.py medium_selection_bias_3 sigmoid_mix_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_3 sigmoid_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_3 sigmoid_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_3 sigmoid_mix_mechanism RL_BIC
+
+# Sigmoid Mix Mech unfaithful 1
+python ./../run_test_vm.py medium_unfaithful_1 sigmoid_mix_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_1 sigmoid_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_1 sigmoid_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_1 sigmoid_mix_mechanism RL_BIC
+
+# Sigmoid Mix Mech unfaithful 2
+python ./../run_test_vm.py medium_unfaithful_2 sigmoid_mix_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_2 sigmoid_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_2 sigmoid_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_2 sigmoid_mix_mechanism RL_BIC
+
+# Sigmoid Mix Mech unfaithful 3
+python ./../run_test_vm.py medium_unfaithful_3 sigmoid_mix_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_3 sigmoid_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_3 sigmoid_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_3 sigmoid_mix_mechanism RL_BIC
+
+# Sigmoid Mix Mech confounder 1
+python ./../run_test_vm.py medium_confounder_1 sigmoid_mix_mechanism SAM
+python ./../run_test_vm.py medium_confounder_1 sigmoid_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_1 sigmoid_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_1 sigmoid_mix_mechanism RL_BIC
+
+# Sigmoid Mix Mech confounder 2
+python ./../run_test_vm.py medium_confounder_2 sigmoid_mix_mechanism SAM
+python ./../run_test_vm.py medium_confounder_2 sigmoid_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_2 sigmoid_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_2 sigmoid_mix_mechanism RL_BIC
+
+# Sigmoid Mix Mech confounder 3
+python ./../run_test_vm.py medium_confounder_3 sigmoid_mix_mechanism SAM
+python ./../run_test_vm.py medium_confounder_3 sigmoid_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_3 sigmoid_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_3 sigmoid_mix_mechanism RL_BIC
+
+# GP Add Mech
+# GP Add Mech Select Bias 1
+python ./../run_test_vm.py medium_selection_bias_1 gp_add_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_1 gp_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_1 gp_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_1 gp_add_mechanism RL_BIC
+
+# GP Add Mech Select Bias 2
+python ./../run_test_vm.py medium_selection_bias_2 gp_add_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_2 gp_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_2 gp_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_2 gp_add_mechanism RL_BIC
+
+# GP Add Mech Select Bias 3
+python ./../run_test_vm.py medium_selection_bias_3 gp_add_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_3 gp_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_3 gp_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_3 gp_add_mechanism RL_BIC
+
+# GP Add Mech unfaithful 1
+python ./../run_test_vm.py medium_unfaithful_1 gp_add_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_1 gp_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_1 gp_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_1 gp_add_mechanism RL_BIC
+
+# GP Add Mech unfaithful 2
+python ./../run_test_vm.py medium_unfaithful_2 gp_add_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_2 gp_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_2 gp_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_2 gp_add_mechanism RL_BIC
+
+# GP Add Mech unfaithful 3
+python ./../run_test_vm.py medium_unfaithful_3 gp_add_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_3 gp_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_3 gp_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_3 gp_add_mechanism RL_BIC
+
+# GP Add Mech confounder 1
+python ./../run_test_vm.py medium_confounder_1 gp_add_mechanism SAM
+python ./../run_test_vm.py medium_confounder_1 gp_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_1 gp_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_1 gp_add_mechanism RL_BIC
+
+# GP Add Mech confounder 2
+python ./../run_test_vm.py medium_confounder_2 gp_add_mechanism SAM
+python ./../run_test_vm.py medium_confounder_2 gp_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_2 gp_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_2 gp_add_mechanism RL_BIC
+
+# GP Add Mech confounder 3
+python ./../run_test_vm.py medium_confounder_3 gp_add_mechanism SAM
+python ./../run_test_vm.py medium_confounder_3 gp_add_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_3 gp_add_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_3 gp_add_mechanism RL_BIC
+
+# GP Mix Mech
+# GP Mix Mech Select Bias 1
+python ./../run_test_vm.py medium_selection_bias_1 gp_mix_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_1 gp_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_1 gp_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_1 gp_mix_mechanism RL_BIC
+
+# GP Mix Mech Select Bias 2
+python ./../run_test_vm.py medium_selection_bias_2 gp_mix_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_2 gp_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_2 gp_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_2 gp_mix_mechanism RL_BIC
+
+# GP Mix Mech Select Bias 3
+python ./../run_test_vm.py medium_selection_bias_3 gp_mix_mechanism SAM
+python ./../run_test_vm.py medium_selection_bias_3 gp_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_selection_bias_3 gp_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_selection_bias_3 gp_mix_mechanism RL_BIC
+
+# GP Mix Mech unfaithful 1
+python ./../run_test_vm.py medium_unfaithful_1 gp_mix_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_1 gp_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_1 gp_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_1 gp_mix_mechanism RL_BIC
+
+# GP Mix Mech unfaithful 2
+python ./../run_test_vm.py medium_unfaithful_2 gp_mix_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_2 gp_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_2 gp_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_2 gp_mix_mechanism RL_BIC
+
+# GP Mix Mech unfaithful 3
+python ./../run_test_vm.py medium_unfaithful_3 gp_mix_mechanism SAM
+python ./../run_test_vm.py medium_unfaithful_3 gp_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_unfaithful_3 gp_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_unfaithful_3 gp_mix_mechanism RL_BIC
+
+# GP Mix Mech confounder 1
+python ./../run_test_vm.py medium_confounder_1 gp_mix_mechanism SAM
+python ./../run_test_vm.py medium_confounder_1 gp_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_1 gp_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_1 gp_mix_mechanism RL_BIC
+
+# GP Mix Mech confounder 2
+python ./../run_test_vm.py medium_confounder_2 gp_mix_mechanism SAM
+python ./../run_test_vm.py medium_confounder_2 gp_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_2 gp_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_2 gp_mix_mechanism RL_BIC
+
+# GP Mix Mech confounder 3
+python ./../run_test_vm.py medium_confounder_3 gp_mix_mechanism SAM
+python ./../run_test_vm.py medium_confounder_3 gp_mix_mechanism DAG_GNN
+python ./../run_test_vm.py medium_confounder_3 gp_mix_mechanism GRAN_DAG
+python ./../run_test_vm.py medium_confounder_3 gp_mix_mechanism RL_BIC
