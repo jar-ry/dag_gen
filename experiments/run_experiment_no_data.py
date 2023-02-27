67bcf387900edc6e009b05f7ce7360b8b5a161be
JayJayc <Jarrry.c@gmail.com>
2023-02-27 22:03:30 +1100

    script for no data gen

--- experiments/run_experiment_no_data.py
+++ experiments/run_experiment_no_data.py
@@ -1,9 +1,7 @@
-from generate_data import generate_data
 from run_test import run_test
 from run_metrics import run_metrics
 
 if __name__ == "__main__":
     print(__name__)
-    generate_data()
     run_test()
     run_metrics()
