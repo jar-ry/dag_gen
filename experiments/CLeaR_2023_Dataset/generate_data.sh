eeeeb5120d43af39b19262243d0e13925d43a21d
JayJayc <Jarrry.c@gmail.com>
2023-03-10 20:10:31 +1100

    Updated generate data script

--- experiments/CLeaR_2023_Dataset/generate_data.sh
+++ experiments/CLeaR_2023_Dataset/generate_data.sh
@@ -7,3 +7,13 @@ python ./../generate_data.py dense_data_config_all_issues_linear.json
 python ./../generate_data.py dense_data_config_confounder_linear.json
 python ./../generate_data.py dense_data_config_selection_bias_linear.json
 python ./../generate_data.py dense_data_config_unfaithful_linear.json
+
+python ./../generate_data.py data_config_all_issues.json
+python ./../generate_data.py data_config_confounder.json
+python ./../generate_data.py data_config_selection_bias.json
+python ./../generate_data.py data_config_unfaithful.json
+
+python ./../generate_data.py data_config_all_issues_linear.json
+python ./../generate_data.py data_config_confounder_linear.json
+python ./../generate_data.py data_config_selection_bias_linear.json
+python ./../generate_data.py data_config_unfaithful_linear.json
