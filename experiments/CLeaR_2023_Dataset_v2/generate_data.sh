e12c8fd90ff868ba03f120eed9f0edc35b9d49db
Jarry Chen <jarrry.c@gmail.com>
2023-04-03 22:35:43 +1000

    Update json

--- experiments/CLeaR_2023_Dataset_v2/generate_data.sh
+++ experiments/CLeaR_2023_Dataset_v2/generate_data.sh
@@ -1,19 +1,23 @@
 python ./../generate_data.py dense_data_config_all_issues.json
+python ./../generate_data.py dense_data_config_no_issue.json
 python ./../generate_data.py dense_data_config_confounder.json
 python ./../generate_data.py dense_data_config_selection_bias.json
 python ./../generate_data.py dense_data_config_unfaithful.json
 
 python ./../generate_data.py dense_data_config_all_issues_linear.json
+python ./../generate_data.py dense_data_config_no_issue_linear.json
 python ./../generate_data.py dense_data_config_confounder_linear.json
 python ./../generate_data.py dense_data_config_selection_bias_linear.json
 python ./../generate_data.py dense_data_config_unfaithful_linear.json
 
 python ./../generate_data.py data_config_all_issues.json
+python ./../generate_data.py data_config_no_issue.json
 python ./../generate_data.py data_config_confounder.json
 python ./../generate_data.py data_config_selection_bias.json
 python ./../generate_data.py data_config_unfaithful.json
 
 python ./../generate_data.py data_config_all_issues_linear.json
+python ./../generate_data.py data_config_no_issue_linear.json
 python ./../generate_data.py data_config_confounder_linear.json
 python ./../generate_data.py data_config_selection_bias_linear.json
 python ./../generate_data.py data_config_unfaithful_linear.json
