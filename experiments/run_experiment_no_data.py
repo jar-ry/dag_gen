a0590aaf178dd04aef82f6e6f22d15e1a8093044
JayJayc <Jarrry.c@gmail.com>
2023-04-25 21:52:27 +1000

    Added logging to metrics

--- experiments/run_experiment_no_data.py
+++ experiments/run_experiment_no_data.py
@@ -1,7 +1,12 @@
 from run_test import run_test
 from run_metrics import run_metrics
+import time
 
 if __name__ == "__main__":
     print(__name__)
+    ts_test = time.time()
+    print("Test time: ", ts_test)
     run_test()
+    ts_metric = time.time()
+    print("Metric time: ", ts_metric)
     run_metrics()
