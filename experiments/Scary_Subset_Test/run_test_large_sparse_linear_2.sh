67c2b31d6eb440873c7b62563024c267a025c211
JayJayc <Jarrry.c@gmail.com>
2023-05-11 22:47:32 +1000

    Set up scary subset tests

---
+++ experiments/Scary_Subset_Test/run_test_large_sparse_linear_2.sh
@@ -0,0 +1,44 @@
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py large_linear_all_issues_1 linear_mechanism SAM
+python ./../run_test_vm.py large_linear_all_issues_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py large_linear_all_issues_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py large_linear_all_issues_1 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py large_linear_all_issues_2 linear_mechanism SAM
+python ./../run_test_vm.py large_linear_all_issues_2 linear_mechanism DAG_GNN
+python ./../run_test_vm.py large_linear_all_issues_2 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py large_linear_all_issues_2 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py large_linear_all_issues_3 linear_mechanism SAM
+python ./../run_test_vm.py large_linear_all_issues_3 linear_mechanism DAG_GNN
+python ./../run_test_vm.py large_linear_all_issues_3 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py large_linear_all_issues_3 linear_mechanism RL_BIC
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py large_linear_confounder_1 linear_mechanism SAM
+python ./../run_test_vm.py large_linear_confounder_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py large_linear_confounder_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py large_linear_confounder_1 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py large_linear_confounder_2 linear_mechanism SAM
+python ./../run_test_vm.py large_linear_confounder_2 linear_mechanism DAG_GNN
+python ./../run_test_vm.py large_linear_confounder_2 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py large_linear_confounder_2 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py large_linear_confounder_3 linear_mechanism SAM
+python ./../run_test_vm.py large_linear_confounder_3 linear_mechanism DAG_GNN
+python ./../run_test_vm.py large_linear_confounder_3 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py large_linear_confounder_3 linear_mechanism RL_BIC
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py large_linear_no_issue_1 linear_mechanism SAM
+python ./../run_test_vm.py large_linear_no_issue_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py large_linear_no_issue_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py large_linear_no_issue_1 linear_mechanism RL_BIC
---
+++ experiments/Scary_Subset_Test/run_test_small_dense_linear.sh
@@ -0,0 +1,94 @@
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py dense_small_linear_all_issues_1 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_all_issues_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_all_issues_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_all_issues_1 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py dense_small_linear_all_issues_2 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_all_issues_2 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_all_issues_2 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_all_issues_2 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py dense_small_linear_all_issues_3 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_all_issues_3 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_all_issues_3 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_all_issues_3 linear_mechanism RL_BIC
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py dense_small_linear_confounder_1 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_confounder_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_confounder_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_confounder_1 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py dense_small_linear_confounder_2 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_confounder_2 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_confounder_2 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_confounder_2 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py dense_small_linear_confounder_3 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_confounder_3 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_confounder_3 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_confounder_3 linear_mechanism RL_BIC
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py dense_small_linear_no_issue_1 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_no_issue_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_no_issue_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_no_issue_1 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py dense_small_linear_no_issue_2 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_no_issue_2 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_no_issue_2 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_no_issue_2 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py dense_small_linear_no_issue_3 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_no_issue_3 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_no_issue_3 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_no_issue_3 linear_mechanism RL_BIC
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py dense_small_linear_selection_bias_1 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_selection_bias_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_selection_bias_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_selection_bias_1 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py dense_small_linear_selection_bias_2 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_selection_bias_2 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_selection_bias_2 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_selection_bias_2 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py dense_small_linear_selection_bias_3 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_selection_bias_3 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_selection_bias_3 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_selection_bias_3 linear_mechanism RL_BIC
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py dense_small_linear_unfaithful_1 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_unfaithful_1 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_unfaithful_1 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_unfaithful_1 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py dense_small_linear_unfaithful_2 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_unfaithful_2 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_unfaithful_2 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_unfaithful_2 linear_mechanism RL_BIC
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py dense_small_linear_unfaithful_3 linear_mechanism SAM
+python ./../run_test_vm.py dense_small_linear_unfaithful_3 linear_mechanism DAG_GNN
+python ./../run_test_vm.py dense_small_linear_unfaithful_3 linear_mechanism GRAN_DAG
+python ./../run_test_vm.py dense_small_linear_unfaithful_3 linear_mechanism RL_BIC
