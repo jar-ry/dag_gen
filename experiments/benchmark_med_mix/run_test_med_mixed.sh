2c9cbb2ad7bfd6e4ecb277f6aa112734a69c85e1
JayJayc <Jarrry.c@gmail.com>
2023-05-22 01:09:20 +1000

    Added med mix benchmark

---
+++ experiments/benchmark_med_mix/run_test_med_mixed.sh
@@ -0,0 +1,191 @@
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py dense_med_mixed_all_issues_1 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_all_issues_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_all_issues_1 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_all_issues_1 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py dense_med_mixed_all_issues_2 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_all_issues_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_all_issues_2 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_all_issues_2 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py dense_med_mixed_all_issues_3 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_all_issues_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_all_issues_3 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_all_issues_3 mix_mechanism NOTEARS
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py dense_med_mixed_confounder_1 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_confounder_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_confounder_1 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_confounder_1 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py dense_med_mixed_confounder_2 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_confounder_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_confounder_2 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_confounder_2 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py dense_med_mixed_confounder_3 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_confounder_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_confounder_3 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_confounder_3 mix_mechanism NOTEARS
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py dense_med_mixed_no_issue_1 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_no_issue_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_no_issue_1 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_no_issue_1 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py dense_med_mixed_no_issue_2 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_no_issue_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_no_issue_2 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_no_issue_2 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py dense_med_mixed_no_issue_3 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_no_issue_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_no_issue_3 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_no_issue_3 mix_mechanism NOTEARS
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py dense_med_mixed_selection_bias_1 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_selection_bias_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_selection_bias_1 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_selection_bias_1 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py dense_med_mixed_selection_bias_2 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_selection_bias_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_selection_bias_2 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_selection_bias_2 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py dense_med_mixed_selection_bias_3 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_selection_bias_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_selection_bias_3 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_selection_bias_3 mix_mechanism NOTEARS
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py dense_med_mixed_unfaithful_1 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_unfaithful_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_unfaithful_1 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_unfaithful_1 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py dense_med_mixed_unfaithful_2 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_unfaithful_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_unfaithful_2 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_unfaithful_2 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py dense_med_mixed_unfaithful_3 mix_mechanism SAM
+python ./../run_test_vm.py dense_med_mixed_unfaithful_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py dense_med_mixed_unfaithful_3 mix_mechanism CAM
+python ./../run_test_vm.py dense_med_mixed_unfaithful_3 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py med_mixed_no_issue_2 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_no_issue_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_no_issue_2 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_no_issue_2 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py med_mixed_no_issue_3 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_no_issue_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_no_issue_3 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_no_issue_3 mix_mechanism NOTEARS
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py med_mixed_selection_bias_1 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_selection_bias_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_selection_bias_1 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_selection_bias_1 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py med_mixed_selection_bias_2 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_selection_bias_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_selection_bias_2 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_selection_bias_2 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py med_mixed_selection_bias_3 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_selection_bias_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_selection_bias_3 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_selection_bias_3 mix_mechanism NOTEARS
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py med_mixed_unfaithful_1 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_unfaithful_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_unfaithful_1 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_unfaithful_1 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py med_mixed_unfaithful_2 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_unfaithful_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_unfaithful_2 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_unfaithful_2 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py med_mixed_unfaithful_3 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_unfaithful_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_unfaithful_3 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_unfaithful_3 mix_mechanism NOTEARS
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py med_mixed_all_issues_1 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_all_issues_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_all_issues_1 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_all_issues_1 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py med_mixed_all_issues_2 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_all_issues_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_all_issues_2 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_all_issues_2 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py med_mixed_all_issues_3 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_all_issues_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_all_issues_3 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_all_issues_3 mix_mechanism NOTEARS
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py med_mixed_confounder_1 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_confounder_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_confounder_1 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_confounder_1 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 2
+python ./../run_test_vm.py med_mixed_confounder_2 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_confounder_2 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_confounder_2 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_confounder_2 mix_mechanism NOTEARS
+
+# Mix Mech Select Bias 3
+python ./../run_test_vm.py med_mixed_confounder_3 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_confounder_3 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_confounder_3 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_confounder_3 mix_mechanism NOTEARS
+
+# Mix Mech
+# Mix Mech Select Bias 1
+python ./../run_test_vm.py med_mixed_no_issue_1 mix_mechanism SAM
+python ./../run_test_vm.py med_mixed_no_issue_1 mix_mechanism DAG_GNN
+python ./../run_test_vm.py med_mixed_no_issue_1 mix_mechanism CAM
+python ./../run_test_vm.py med_mixed_no_issue_1 mix_mechanism NOTEARS
+
+
